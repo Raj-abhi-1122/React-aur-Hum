e7621a1cab9274c6eda9a7422b37e3d013168166
Rajabhi1122 <rajabhishek4anh@gmail.com>
2026-04-03 00:22:39 +0530

    02 lecture of CHAI AUR REACT SERIS

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React | ABHISHEK RAJ </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+   <h1>Chai aur React | with VITE | ABHISHEK Raj </h1>
+  )
+}
+
+export default App
